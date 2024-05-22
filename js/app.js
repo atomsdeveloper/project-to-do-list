286342650d392c658016a6d27167cdc76acca2ea
renanns <atomsfrontenf@gmail.com>
2024-05-21 22:00:26 -0300

    feat: refactor logic to delete task and complet task

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const addBtn = document.querySelector('button#newTaskBtn')
 
 const tasks = document.querySelector('div.newTasks')
 
-const checkInput = () => inputElement.value.trim().length > 0;  // callbackFunction, retorna se a o valor recebido em 'inputElement', sem espaços com o 'trim()' é maior que 0. 
 
 const handleAddTask = () => {
     const isValid = checkInput();
@@ -18,42 +17,42 @@ const handleAddTask = () => {
     const text = document.createElement('p');
     text.innerText = inputElement.value;
 
-    text.addEventListener('click', () => textClick(text));
+    text.addEventListener('click', () => handleTextClick());
 
     const deleteItem = document.createElement('i');
 
     deleteItem.classList.add('fa-solid');
     deleteItem.classList.add('fa-trash');
 
-    newTasksItem.addEventListener('click', () => deleteClick(text, newTasksItem));
+    newTasksItem.addEventListener('click', () => handleDeleteClick());
 
     newTasksItem.appendChild(text);
     newTasksItem.appendChild(deleteItem);
 
     tasks.appendChild(newTasksItem)
-
+    
     inputElement.value = "";
 };
 
-const textClick = (text) => {
-    const tasks = newTasksItem.childNodes;
+const handleTextClick = () => {
+    const tasks = document.getElementsByClassName('tasksItem')
 
-    for (const task of tasks) {
-        if (task.firstChild.isSameNode(text)) {
-            task.firstChild.classList.toggle("completed");
-        };
-    };
+    for(let task of tasks) {
+        task.firstChild.classList.add("completed")
+    }
 };
 
-const deleteClick = (text, newTasksItem) => {
-    const tasks = newTasksItem.childNodes;
+const handleDeleteClick = () => {
+    const tasks = document.getElementsByClassName('tasksItem')
 
-    for (const task of tasks) {
-        if (task.firstChild.isSameNode(text)) {
-            newTasksItem.remove();
-        };
-    };
+    for(let task of tasks) {    
+        task.children[1].addEventListener("click", () => {
+            task.remove()
+        })
+    }
 };
+
+const checkInput = () => inputElement.value.trim().length > 0;  // callbackFunction, retorna se a o valor recebido em 'inputElement', sem espaços com o 'trim()' é maior que 0. 
 
 const handleAddTaskChange = () => {
     const isValid = checkInput();
